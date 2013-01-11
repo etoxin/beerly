d2ba0574df0c8f674067a521f6eeca810949f873
etoxin <adam@etoxin.net>
2013-01-11 21:01:54 +1100

    Added grunt.js
    Added grunt js.

---
+++ source/lib/beerly.js
@@ -0,0 +1,15 @@
+/*
+ * beerly
+ * http://beerly.com.au
+ *
+ * Copyright (c) 2013 Adam Lusted
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
